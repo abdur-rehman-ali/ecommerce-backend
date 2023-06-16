d1c03591b36d1eadf56ad44bf54fe2d68912f04b
abdur-rehman-ali <alijoyia911@gmal.com>
2023-06-16 17:26:15 -0400

    Add filters for brand, category and sort

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,13 +2,24 @@ import Product from "../model/Product.js";
 
 class ProductsController {
   static fetchAllProducts = async (req, res) => {
+    let query = Product.find()
+    if (req.query.brand) {
+      query = query.find({ brand: req.query.brand })
+    }
+    if (req.query.category) {
+      query = query.find({ category: req.query.category })
+    }
+    if (req.query.sort) {
+      query = query.sort({ [req.query.sort]: req.query.order ? req.query.order : 'ASC' })
+    }
     try {
-      const products = await Product.find()
+      const products = await query
       res.status(200).json(products)
     } catch (error) {
       res.status(400).send(error)
     }
   }
+
   static createProduct = async (req, res) => {
     const product = new Product(req.body)
     try {
@@ -24,7 +35,7 @@ class ProductsController {
     try {
       const product = await Product.findById(id);
       if (!product) {
-        return res.status(404).json({message: `No Product exist with id: ${id}` })
+        return res.status(404).json({ message: `No Product exist with id: ${id}` })
       }
       res.status(200).json(product)
     } catch (error) {
